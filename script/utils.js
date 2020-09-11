f50172376df1b945b2b476af6f6b700a6b898914
ysds <ysds.code@gmail.com>
2020-09-11 23:53:56 +0900

    Separate utils.js

---
+++ script/utils.js
@@ -0,0 +1,40 @@
+module.exports = {
+  hiraToKana: function(str) {
+    return str.replace(/[\u3041-\u3096]/g, function(match) {
+      const chr = match.charCodeAt(0) + 0x60;
+      return String.fromCharCode(chr);
+    });
+  },
+  daku_conv: function(str) {
+    str = str.normalize("NFD");
+    str = str.replace(/[\u3099\u309A]/g, "");
+    return str;
+  },
+  choon_conv: function(str) {
+    let a = str.substr(0, 1);
+    for (var i = 1; i < str.length; i++) {
+      let t = str.substr(i, 1);
+      if (t == "ー") {
+        const mae = str.substr(i - 1, 1);
+        if (mae.match(/[アカサタナハマヤラワガザダバパァャ]/)) {
+          t = "ア";
+        } else if (mae.match(/[イキシチニヒミリギジヂビピィ]/)) {
+          t = "イ";
+        } else if (mae.match(/[ウクスツヌフムユルグズヅブプゥュ]/)) {
+          t = "ウ";
+        } else if (mae.match(/[エケセテネヘメレゲゼデベペェ]/)) {
+          t = "エ";
+        } else if (mae.match(/[オコソトノホモヨロゴゾドボポォョ]/)) {
+          t = "オ";
+        }
+      }
+      a += t;
+    }
+
+    return a;
+  },
+  tsu_conv: function(str) {
+    str = str.replace("ッ", "ツ");
+    return str;
+  }
+};
